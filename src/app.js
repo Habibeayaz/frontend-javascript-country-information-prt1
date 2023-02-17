9221f0bf1bccd1118d12180307f53957815d5cfc
Habibeayaz <habibe_ayaz@hotmail.com>
2023-02-17 19:16:11 +0100

    uitwerkingen

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('Hallo daar!');
+import axios from 'axios';
+
+console.log("hallo daar!");
+
+async function fetchData () {
+    try {
+        const response = await axios.get("https://restcountries.com/v2/all");
+        console.log(response.data);
+    } catch (e) {
+        console.error(e);
+    }
+}
+fetchData();
+
+const firstCountry = countries.find((country) => {
+    return (country.name);
+});
+console.log(firstCountry);
